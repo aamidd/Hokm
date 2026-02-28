0dea8e58e4d5a50140d649077d000da09b241a16
aamidd <sanaiamid@gmail.com>
2026-02-28 20:14:14 +0330

    initialize Card

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Card {
+    private final int suit; // 0: Club, 1: Diamonds, 2: Hearts, 3: Spades
+    private final int rank; // 2 to 14 (ace)
+
+    public Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 }
