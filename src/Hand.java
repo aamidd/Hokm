6334df55ecca50b8e76d4572ad612d4d65ed2a59
aamidd <sanaiamid@gmail.com>
2026-03-02 16:17:20 +0330

    add toHorizontalString

--- src/Hand.java
+++ src/Hand.java
@@ -1,17 +1,41 @@
 import java.util.ArrayList;
 
 public class Hand {
-    private final ArrayList<Card> cardsInHand = new ArrayList<>();
+    private final ArrayList<Card> cards = new ArrayList<>();
 
     public void addCard(Card card) {
-        cardsInHand.add(card);
+        cards.add(card);
     }
 
     public void useCard(Card card) {
-        cardsInHand.remove(card);
+        cards.remove(card);
     }
 
     public void useCard(int cardNumber) {
-        useCard(cardsInHand.get(cardNumber - 1));
+        useCard(cards.get(cardNumber - 1));
+    }
+
+    public String toHorizontalString() {
+        String str = "";
+        int cardsSize = cards.size();
+        for (int i = 1; i <= cardsSize; i++) {
+            str += String.format("%3d   ", i);
+        }
+        str += "\n";
+        for (int i = 0; i < 13; i++) {
+            String up = String.format("⌜%-3s⌝", cards.get(i).getRankStr());
+            str += String.format("%-6s", up);
+        }
+        str += "\n";
+        for (int i = 0; i < 13; i++) {
+            String middle = String.format("  %s  ", cards.get(i).getSuitStr());
+            str += String.format("%-6s", middle);
+        }
+        str += "\n";
+        for (int i = 0; i < 13; i++) {
+            String lower = String.format("⌞%3s⌟", cards.get(i).getRankStr());
+            str += String.format("%-6s", lower);
+        }
+        return str;
     }
 }
