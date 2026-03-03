68552dac3d221d105b0ff3225f69ab3c2cee70d5
aamidd <sanaiamid@gmail.com>
2026-03-03 16:02:25 +0330

    remove toHorizontalString()

--- src/Hand.java
+++ src/Hand.java
@@ -15,30 +15,6 @@ public class Hand {
         useCard(cards.get(cardNumber - 1));
     }
 
-    public String toHorizontalString() {
-        StringBuilder str = new StringBuilder();
-        int cardsSize = cards.size();
-        for (int i = 1; i <= cardsSize; i++) {
-            str.append(String.format("%3d   ", i));
-        }
-        str.append("\n");
-        for (Card card : cards) {
-            String up = String.format("⌜%s%s⌝", card.getRankStr(), card.getRank() == 10 ? "¯" : "¯¯");
-            str.append(String.format("%-6s", up));
-        }
-        str.append("\n");
-        for (Card card : cards) {
-            String middle = String.format("│ %s │", card.getSuitStr());
-            str.append(String.format("%-6s", middle));
-        }
-        str.append("\n");
-        for (Card card : cards) {
-            String lower = String.format("⌞%s%s⌟", card.getRank() == 10 ? "_" : "__", card.getRankStr());
-            str.append(String.format("%-6s", lower));
-        }
-        return str.toString();
-    }
-
     public String toDiagonalString() {
         StringBuilder str = new StringBuilder();
         int size = cards.size();
