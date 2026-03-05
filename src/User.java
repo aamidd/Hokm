2fb7d22306823f90a4c7126d524ac782b6c5e7ed
aamidd <sanaiamid@gmail.com>
2026-03-05 16:18:01 +0330

    create user

---
+++ src/User.java
@@ -0,0 +1,27 @@
+public class User {
+    private static int numberOfUsers = 0;
+    private final int id;
+    private String username;
+
+    public User() {
+        id = numberOfUsers++;
+        username = "Player" + numberOfUsers;
+    }
+
+    public User(String username) {
+        id = numberOfUsers++;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
