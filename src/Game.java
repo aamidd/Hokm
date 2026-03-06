cd1b6991d9e24720ed9803afa5456962ff939d35
aamidd <sanaiamid@gmail.com>
2026-03-06 17:05:35 +0330

    add chooseTeams with a deck of cards

--- src/Game.java
+++ src/Game.java
@@ -6,4 +6,20 @@ public class Game {
     public void addUser(User user) {
         users.add(user);
     }
+
+    public void chooseTeams(Deck deck) {
+        int firstAce = 51 - deck.find("A");
+        int secondAce = 51 - deck.find("A", 2);
+
+        int firstUserIndex = firstAce % 4;
+        int secondUserIndex = secondAce % 4;
+
+        User tmp = users.get(firstUserIndex);
+        User tmp2 = users.get(secondUserIndex);
+
+        users.set(firstUserIndex, users.getFirst());
+        users.set(0, tmp);
+        users.set(secondUserIndex, users.get(2));
+        users.set(2, tmp2);
+    }
 }
