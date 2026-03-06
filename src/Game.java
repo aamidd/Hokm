3964790624c0729bb889eb44e68ad41de0ab3214
aamidd <sanaiamid@gmail.com>
2026-03-06 16:55:21 +0330

    add addUser

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Game {
+    ArrayList<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
 }
