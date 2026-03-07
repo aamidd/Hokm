55e515fb61079c94902836cc5f16d6dd60dcb9b9
aamidd <sanaiamid@gmail.com>
2026-03-07 07:26:03 +0330

    add players

--- src/HokmCLI.java
+++ src/HokmCLI.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class HokmCLI {
     public void run() {
+        System.out.println("Welcome to Hokm!\n");
 
-
+        Hokm game = new Hokm();
+        for (int i = 1; i <= 4; i++) {
+            String username = getInput(String.format("Player %d: ", i));
+            User user = new User(username);
+            game.addUser(user);
+        }
     }
 
     public String getInput(String prompt) {
