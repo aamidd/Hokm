1918c7e507fd20dd0a807f1b79fc40b481cff3f0
aamidd <sanaiamid@gmail.com>
2026-06-15 20:12:39 +0330

    add addCard

--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,14 @@
 import java.util.ArrayList;
 
 public class Table {
+    private final ArrayList<Card> cards = new ArrayList<>(); // played cards
+    private final int hokm = -1;
+
+    public void addCard(Card card) {
+        if (cards.size() >= 4) {
+            throw new IllegalStateException("Table is full. max cards on the table is 4.");
+        } else {
+            cards.add(card);
+        }
+    }
 }
