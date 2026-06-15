16841e36df323f58f195aeac0634757986b231b6
aamidd <sanaiamid@gmail.com>
2026-06-15 20:25:42 +0330

    add zamine

--- src/Table.java
+++ src/Table.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Table {
     private final ArrayList<Card> cards = new ArrayList<>(); // played cards
     private int hokm = -1;
+    private int zamine = -1;
 
     public void addCard(Card card) {
         if (cards.size() >= 4) {
@@ -17,5 +18,12 @@ public class Table {
             throw new IllegalArgumentException("Hokm should be between 0-3");
         }
         this.hokm = hokm;
+    }
+
+    public void setZamine(int zamine) {
+        if (zamine > 3 || zamine < 0) {
+            throw new IllegalArgumentException("Zamine should be between 0-3");
+        }
+        this.zamine = zamine;
     }
 }
